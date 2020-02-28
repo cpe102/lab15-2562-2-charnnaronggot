572a5b204397646f487c1bb3c5faa6e8c4c95e91
charnnaronggot <charnnarong_2542@hotmail.com>
2020-02-28 16:48:35 +0700

    LAB15

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -23,5 +23,36 @@ int main(){
 	findRowSum(dPtr,result,N,M);
 	showData(result,N,1);
 }
+void randData(double *dPtr,int N,int M){
+	for(int i = 0 ; i < N ; i ++){
+		for(int j = 0 ; j < M ; j++){
+			double s = rand()%101 ;
+			*(dPtr+(M*i)+j) = s / 100 ;
+			
+		}
+	}
+	
+}
+void findRowSum(const double *dPtr,double *result,int N,int M){
+for(int i = 0 ; i < N ; i ++){
+		for(int j = 0 ; j < M ; j++){
+			*(result+i) += *(dPtr+(M*i)+j) ;
+		}
+	}
+}
+void showData(double *dPtr,int N,int M){
+	for(int i = 0 ; i < N ; i ++){
+		cout << "\n" ;
+		for(int j = 0 ; j < M ; j++){
+			
+			cout << *(dPtr+(M*i)+j)<< " "  ;
+			
+		}
+	}
+	cout << endl ;
+}
+
+	
 
 //Write definition of randData(), showData() and findRowSum()
+// srand()%0.00/1.00 
